c8d79b15a0d6fa9c5863b24647b989bbed286b74
Lori Boyd <lori.p.boyd@gmail.com>
2020-03-22 19:23:03 -0400

    separates ui from app.js

--- flickpix-frontend/src/App.js
+++ flickpix-frontend/src/App.js
@@ -1,21 +1,10 @@
-import PropTypes from 'prop-types';
 import React from 'react';
-import {Route, Switch, Link, NavLink} from 'react-router-dom'
-import MovieContainer from './components/MovieContainer';
-import VerticalSidebar from './components/VerticalSidebar';
 import Home from './components/Home';
-import {
-  Segment,
-  Sidebar,
-} from 'semantic-ui-react';
+
 let api = `http://localhost:4000/movies`;
 
 export default class app extends React.Component {
   state = {
-    animation: 'overlay',
-    direction: 'left',
-    dimmed: false,
-    visible: false,
     movies: [],
     users: [],
     queues: []
@@ -31,42 +20,10 @@ export default class app extends React.Component {
     }))
   }
 
-  handleAnimationChange = (animation) => () =>
-    this.setState((prevState) => ({ animation, visible: !prevState.visible }))
-
-  handleDimmedChange = (e, { checked }) => this.setState({ dimmed: checked })
-
-  handleDirectionChange = (direction) => () =>
-    this.setState({ direction, visible: false })
-
   render() {
-    const { animation, dimmed, direction, visible } = this.state
-    const vertical = direction === 'bottom' || direction === 'top'
     console.log(this.state)
-
     return (
-      <div>
-        <h1 className="ui orange center aligned header">
-          FlickPix
-        </h1>
-        <i className="bars icon" onClick={this.handleAnimationChange('scale down')}></i>
-
-        <Sidebar.Pushable as={Segment}>
-          {vertical ? null : (
-            <VerticalSidebar
-              animation={animation}
-              direction={direction}
-              visible={visible}
-            />
-          )}
-
-          <Sidebar.Pusher dimmed={dimmed && visible}>
-            <Switch>
-              <Route path="/" exact component={ Home } />
-            </Switch>
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-      </div>
+      <Home />
     )
   }
 }
--- flickpix-frontend/src/components/Home.js
+++ flickpix-frontend/src/components/Home.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import {Route, Switch, Link, NavLink} from 'react-router-dom'
+import MovieContainer from './MovieContainer';
+import VerticalSidebar from './VerticalSidebar';
+import {
+  Segment,
+  Sidebar,
+} from 'semantic-ui-react';
 
-const Home = () => (
-  <div className="ui container">
-    <div className="ui text container">
-      <button className="ui fluid red massive button">Get a random Movie!</button>
-    </div>
-  </div>
-);
+export default class Home extends React.Component {
+  state = {
+    animation: 'overlay',
+    direction: 'left',
+    dimmed: false,
+    visible: false,
+  }
 
-export default Home;
+  handleAnimationChange = (animation) => () =>
+    this.setState((prevState) => ({ animation, visible: !prevState.visible }))
+
+  handleDimmedChange = (e, { checked }) => this.setState({ dimmed: checked })
+
+  handleDirectionChange = (direction) => () =>
+    this.setState({ direction, visible: false })
+
+  render() {
+    const { animation, dimmed, direction, visible } = this.state
+    const vertical = direction === 'bottom' || direction === 'top'
+
+    return (
+      <div>
+        <h1 className="ui orange center aligned header">
+          FlickPix
+        </h1>
+        <i className="bars icon" onClick={this.handleAnimationChange('scale down')}></i>
+
+        <Sidebar.Pushable as={Segment}>
+          {vertical ? null : (
+            <VerticalSidebar
+              animation={animation}
+              direction={direction}
+              visible={visible}
+            />
+          )}
+
+          <Sidebar.Pusher dimmed={dimmed && visible}>
+            <div className="ui container">
+              <div className="ui text container">
+                <button className="ui fluid red massive button">Get a random Movie!</button>
+              </div>
+            </div>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    )
+  }
+}
